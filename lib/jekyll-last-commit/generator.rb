251be86e7333a3ba3370c538dc4d9ec14d849e59
Kip Landergren <klandergren@users.noreply.github.com>
2022-12-16 14:04:32 -0800

    start work on reading date format from config

--- lib/jekyll-last-commit/generator.rb
+++ lib/jekyll-last-commit/generator.rb
@@ -9,21 +9,30 @@ module JekyllLastCommit
       repo_man.discover_commits(site.documents.map {|d| d.relative_path })
       repo_man.discover_commits(site.pages.map {|p| p.relative_path })
 
+      date_format = site.config.dig('jekyll-last-commit', 'date-format')
+      date_format ||= '%B %d, %Y'
+
       site.documents.each do |document|
         commit = repo_man.find_commit(document.relative_path)
 
+        raw_time = Time.at(commit["time"].to_i)
+
         unless commit.nil?
           document.data['last_commit'] = commit
-          document.data['last_modified_at'] = Time.at(commit["time"].to_i)
+          document.data['last_modified_at'] = raw_time
+          document.data['last_modified_at_formatted'] = raw_time.strftime(date_format)
         end
       end
 
       site.pages.each do |page|
         commit = repo_man.find_commit(page.relative_path)
 
+        raw_time = Time.at(commit["time"].to_i)
+
         unless commit.nil?
           page.data['last_commit'] = commit
-          page.data['last_modified_at'] = Time.at(commit["time"].to_i)
+          page.data['last_modified_at'] = raw_time
+          page.data['last_modified_at_formatted'] = raw_time.strftime(date_format)
         end
       end
     end

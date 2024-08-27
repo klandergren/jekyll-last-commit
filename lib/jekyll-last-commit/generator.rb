c1df0415273de3e0f1c0452d93dd934f62b52e0c
“Martin <mporcheron@users.noreply.github.com>
2024-08-27 11:55:25 +0100

    Add information about data files to site.data.meta

--- lib/jekyll-last-commit/generator.rb
+++ lib/jekyll-last-commit/generator.rb
@@ -6,11 +6,28 @@ module JekyllLastCommit
     def generate(site)
       repo_man = JekyllLastCommit::RepoMan.new(site.source)
       repo_man.discover_repo()
+
+      data_file_extensions = ['.yml', '.yaml', '.json', '.tsv', '.csv']
+      data_files = []
+      site.data.keys.each do |data_file|
+        data_file_extensions.each do |data_file_extension|
+          data_file_name = data_file + data_file_extension
+          if File.file?('./_data/' + data_file_name)
+            data_files.append(data_file_name)
+            break
+          end
+        end
+      end     
+
       repo_man.discover_commits(site.documents.map {|d| d.relative_path })
       repo_man.discover_commits(site.pages.map {|p| p.relative_path })
+      repo_man.discover_commits(data_files.map {|df| './_data/' + df })
 
       date_format = site.config.dig('jekyll-last-commit', 'date_format')
       date_format ||= '%B %d, %Y'
+
+      data_file_key = site.config.dig('jekyll-last-commit', 'data_files_key')
+      data_file_key ||= 'meta'
 
       should_fall_back_to_mtime = site.config.dig('jekyll-last-commit', 'should_fall_back_to_mtime')
       should_fall_back_to_mtime = should_fall_back_to_mtime.nil? ? true : should_fall_back_to_mtime
@@ -32,10 +49,34 @@ module JekyllLastCommit
           end
         else
           raw_time = Time.at(commit["time"].to_i)
-
           document.data['last_commit'] = commit
           document.data['last_modified_at'] = raw_time
           document.data['last_modified_at_formatted'] = raw_time.strftime(date_format)
+        end
+      end
+
+      site.data[data_file_key] = {}
+      data_files.each do |data_file|
+        site.data[data_file_key][data_file] = {}
+
+        relative_path = './_data/' + data_file
+        commit = repo_man.find_commit(relative_path)
+
+        if commit.nil?
+          if should_fall_back_to_mtime
+            path_data_file = Jekyll.sanitized_path(site.source, relative_path)
+
+            if File.file?(data_file)
+              raw_time = Time.at(File.mtime(path_data_file).to_i)
+              site.data[data_file_key][data_file]['last_modified_at'] = raw_time
+              site.data[data_file_key][data_file]['last_modified_at_formatted'] = raw_time.strftime(date_format)
+            end
+          end
+        else
+          raw_time = Time.at(commit['time'].to_i)
+          site.data[data_file_key][data_file]['last_commit'] = commit
+          site.data[data_file_key][data_file]['last_modified_at'] = raw_time
+          site.data[data_file_key][data_file]['last_modified_at_formatted'] = raw_time.strftime(date_format)
         end
       end
 
@@ -55,13 +96,11 @@ module JekyllLastCommit
 
         else
           raw_time = Time.at(commit["time"].to_i)
-
           page.data['last_commit'] = commit
           page.data['last_modified_at'] = raw_time
           page.data['last_modified_at_formatted'] = raw_time.strftime(date_format)
         end
       end
     end
-
   end
 end
